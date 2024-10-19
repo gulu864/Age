4104452e99a54a8b7ebbca85908a949e6707921b
gulu864 <suva.kanta4u@gmail.com>
2024-10-19 17:20:18 +0530

    Add files via upload

---
+++ Age.py
@@ -0,0 +1,11 @@
+print("Hi I am Raj and I am going to see if you are allowed to enter my class.")
+y=int(input("Enter your age:"))
+
+if y>10:
+    if y<=20:
+        print("You are allowed.")
+    else:
+        print("You are not allowed.")
+    
+else:
+    print("You are allowed to my class.")
